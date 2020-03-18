1570471e3d61b4a07a01afcd5500b864aadf9a84
Birgit Kruus <bikruu@ttu.ee>
2020-03-18 17:44:41 +0200

    transaction component sends data

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Transaction} from '../Models/Transaction';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {IncomeStatementType} from '../Models/IncomeStatementType';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import {FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {parse} from 'ts-node';
 
 @Component({
   selector: 'app-transactions',
@@ -13,8 +14,39 @@ import {FormGroup} from '@angular/forms';
 export class TransactionsComponent implements OnInit {
   transactionList: Transaction[];
   incomeStatementTypeList: IncomeStatementType[];
+  formGroup: FormGroup;
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      arr: this.formBuilder.array([])
+    });
+  }
+
+  submit() {
+    this.http.post('http://localhost:8080/api/transactions/update',
+      this.transactionList
+      )
+      .subscribe(
+        (val) => {
+          console.log('POST call successful value returned in body',
+            val);
+        },
+        response => {
+          console.log('POST call in error', response);
+        },
+        () => {
+          console.log('The POST observable is now completed. ');
+        });
+
+
+    console.log(this.transactionList);
+  }
 
   ngOnInit(): void {      this.http.get<Transaction[]>('http://localhost:8080/api/transactions/all').subscribe(result => {
     this.transactionList = result;
